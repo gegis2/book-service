0459f6b6629cb240eb928956da391320939ef9b5
gegis2 <mindaugasnavickasss@gmail.com>
2021-01-17 04:45:14 +0200

    added xml ellement atributes to book
    preparing to store and read xml files

--- com.books.services/src/main/java/book_services/Book.java
+++ com.books.services/src/main/java/book_services/Book.java
@@ -1,5 +1,10 @@
 package book_services;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Book {
     private String name;
     private String barCode;
@@ -8,11 +13,6 @@ public class Book {
     private int quantity;
 
     public Book() {
-        name = "";
-        barCode = "";
-        author = "";
-        price = 0.0;
-        quantity = 0;
     }
 
     public Book(String name, String barCode, String author, double price, int quantity) {
@@ -27,6 +27,7 @@ public class Book {
         return this.name;
     }
 
+    @XmlElement
     public void setName(String name) {
         this.name = name;
     }
@@ -35,6 +36,7 @@ public class Book {
         return this.barCode;
     }
 
+    @XmlAttribute
     public void setBarCode(String barCode) {
         this.barCode = barCode;
     }
@@ -43,6 +45,7 @@ public class Book {
         return this.author;
     }
 
+    @XmlElement
     public void setAuthor(String author) {
         this.author = author;
     }
@@ -51,6 +54,7 @@ public class Book {
         return this.price;
     }
 
+    @XmlElement
     public void setPrice(double price) {
         this.price = price;
     }
@@ -59,6 +63,7 @@ public class Book {
         return this.quantity;
     }
 
+    @XmlElement
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
